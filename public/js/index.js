9ee5282f0c7806c5ab7c898165d7b61d9f608bae
noah <noah.sigel@gmail.com>
2021-09-20 19:28:47 -0400

    Cleaned up some styles + removed useless
    :)

--- public/js/index.js
+++ public/js/index.js
@@ -15,40 +15,40 @@ circle.crossOrigin = "anonymous";
 circle.src = "https://i.ibb.co/gT3NyL3/888-Inner-Circle-Maalavidaa-1.png";
 
 const handleNewFile = (file) => {
-  const reader = new FileReader();
+	const reader = new FileReader();
 
-  reader.readAsDataURL(file);
-  reader.onloadend = (event) => {
-    const image = new Image();
-    image.src = event.target.result;
-    image.onload = function () {
-      console.log("Building canvas...");
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      const width = (canvas.width = Math.min(image.width, circle.width));
-      const height = (canvas.height = Math.min(image.height, circle.height));
-      ctx.drawImage(image, 0, 0, width, height);
-      ctx.drawImage(circle, 0, 0, width, height);
-      ctx.globalCompositeOperation = "destination-in";
-      ctx.beginPath();
-      ctx.arc(width / 2, height / 2, height / 2, 0, Math.PI * 2);
-      ctx.closePath();
-      ctx.fill();
-      console.log("Finished building canvas");
-      selectedFile = file;
-    };
-  };
+	reader.readAsDataURL(file);
+	reader.onloadend = (event) => {
+		const image = new Image();
+		image.src = event.target.result;
+		image.onload = function () {
+			console.log("Building canvas...");
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			const width = (canvas.width = Math.min(image.width, circle.width));
+			const height = (canvas.height = Math.min(image.height, circle.height));
+			ctx.drawImage(image, 0, 0, width, height);
+			ctx.drawImage(circle, 0, 0, width, height);
+			ctx.globalCompositeOperation = "destination-in";
+			ctx.beginPath();
+			ctx.arc(width / 2, height / 2, height / 2, 0, Math.PI * 2);
+			ctx.closePath();
+			ctx.fill();
+			console.log("Finished building canvas");
+			selectedFile = file;
+		};
+	};
 };
 
 // Handle new image selection
-fileInput.onchange = function (e) {
-  console.log("Received", e.target.files[0], "from file input");
-  handleNewFile(e.target.files[0]);
+fileInput.onclick = function (e) {
+	console.log("Received", e.target.files[0], "from file input");
+	handleNewFile(e.target.files[0]);
 };
 
 // Handle drag-and-drop
 const preventDefault = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
+	e.preventDefault();
+	e.stopPropagation();
 };
 
 root.addEventListener("dragenter", preventDefault, false);
@@ -57,30 +57,30 @@ root.addEventListener("dragover", preventDefault, false);
 root.addEventListener("drop", preventDefault, false);
 
 root.addEventListener(
-  "drop",
-  (e) => {
-    const file = e.dataTransfer?.files[0];
+	"drop",
+	(e) => {
+		const file = e.dataTransfer?.files[0];
 
-    if (file) {
-      handleNewFile(file);
-    }
-  },
-  false
+		if (file) {
+			handleNewFile(file);
+		}
+	},
+	false
 );
 
 // Handle download
 downloadButton.onclick = function () {
-  console.log("Attemping to download canvas");
-  if (!selectedFile) return console.log("No selected image!");
+	console.log("Attemping to download canvas");
+	if (!selectedFile) return console.log("No selected image!");
 
-  const linkElement = document.createElement("a");
+	const linkElement = document.createElement("a");
 
-  const fileName =
-    selectedFile.name.substring(0, selectedFile.name.lastIndexOf(".")) + ".png";
+	const fileName =
+		selectedFile.name.substring(0, selectedFile.name.lastIndexOf(".")) + ".png";
 
-  console.log("Preparing", fileName, "for download");
-  linkElement.download = fileName;
-  linkElement.href = canvas.toDataURL("image/png");
+	console.log("Preparing", fileName, "for download");
+	linkElement.download = fileName;
+	linkElement.href = canvas.toDataURL("image/png");
 
-  linkElement.click();
+	linkElement.click();
 };
